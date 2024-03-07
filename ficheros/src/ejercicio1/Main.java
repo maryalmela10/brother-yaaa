0501e45aa59172560a63f506795221bfe2efceff
maryalmela10 <maryalmela10@gmail.com>
2024-03-07 21:09:56 +0100

    h

--- ficheros/src/ejercicio1/Main.java
+++ ficheros/src/ejercicio1/Main.java
@@ -13,13 +13,8 @@ public class Main {
 	public static void main(String[] args){
 		Scanner teclado=new Scanner(System.in);
 		PrintWriter ficheroNotas = null;
-		PrintWriter escribir = null;
-		BufferedReader leer = null;
-		BufferedReader lecturaFicheroNotas = null;
-		String valorTeclado, linea;
-		double media, suma=0;
-		int cantidadNotas=0;
-		
+		String valorTeclado;
+
 		//ejercicio 1
 		try {
 		ficheroNotas = new PrintWriter(new FileWriter("notas.txt"));
@@ -36,68 +31,11 @@ public class Main {
 			//para depurar
 			e.printStackTrace();
 		} 
-		//ejercicio 2
-		try {
-			lecturaFicheroNotas = new BufferedReader(new FileReader("notas.txt"));
-			linea=lecturaFicheroNotas.readLine();
-			while(linea!=null) {
-				cantidadNotas++;
-				suma+=Integer.parseInt(linea);
-			}
-			if (cantidadNotas==0) {
-				System.out.println("No hay datos");
-			} else {
-				media=suma/cantidadNotas;
-				//Filtrar y escribir en el fichero
-				escribir = new PrintWriter(new FileWriter("mayores.txt"));
-				leer = new BufferedReader(new FileReader("notas.txt"));
-				linea=leer.readLine();
-				while(linea!=null) {
-					if(Double.parseDouble(linea)>media) {
-						escribir.println(linea);
-					}
-					linea=leer.readLine();
-				}
-				
-				//Mostrar dato
-			}
-			
-		}
 		
-		catch (FileNotFoundException e) {
-			//para depurar
-			e.printStackTrace();
-		}
-		catch (IOException e) {
-			//para depurar
-			e.printStackTrace();
-		} 
 		
 		finally {
 			if(ficheroNotas!=null) {
 				ficheroNotas.close();
-			}
-			
-			if(escribir!=null) {
-				escribir.close();
-			}
-			
-			if(leer!=null) {
-				try {
-					leer.close();
-				} catch (IOException e) {
-					//para depurar
-					e.printStackTrace();
-				} 	
-			}
-			
-			if(lecturaFicheroNotas!=null) {
-				try {
-					lecturaFicheroNotas.close();
-				} catch (IOException e) {
-					//para depurar
-					e.printStackTrace();
-				} 	
 			}
 			
 		}
