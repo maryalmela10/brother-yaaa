6ccea1505a27189c996718b9db3daf98416c8490
maryalmela10 <maryalmela10@gmail.com>
2024-03-11 12:56:51 +0100

    k

--- ficheros/src/ejercicio3/Main.java
+++ ficheros/src/ejercicio3/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		BufferedReader leer = null;
 		BufferedReader leer2 = null;
 		Scanner teclado=new Scanner(System.in);
-		int numero, elemento0, elemento1, elemento2;
+		int numero, elemento0=0, elemento1=0, elemento2=0;
 		String linea0 = null, linea1 = null, linea2 = null;
 		PrintWriter escribir=null;
 		
@@ -31,18 +31,26 @@ public class Main {
 						linea2=leer.readLine();
 						if(linea0==null && linea2!=null) {
 							elemento2=Integer.parseInt(linea2);
+							System.out.println("Valor siguente: "+elemento2);
 						} else if (linea0!=null && linea2==null) {
 							elemento0=Integer.parseInt(linea0);
+							System.out.println("Valor anterior: "+elemento0);
 						} else {
 							elemento2=Integer.parseInt(linea2);
 							elemento0=Integer.parseInt(linea0);
+							System.out.println("Valor anterior: "+elemento0);
+							System.out.println("Valor siguente: "+elemento2);
 						}
 						break;
 					} else {
+						linea1 = leer.readLine();
 						linea0=leer2.readLine();
 					}
 	            }
 			
+			if (elemento0==0 && elemento1==0 && elemento2==0) {
+				System.out.println("Numero no encontrado");
+			}
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -51,7 +59,26 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 
+		
+		finally {
+			if(leer!=null) {
+				try {
+					leer.close();
+				} catch (IOException e) {
+					//para depurar
+					e.printStackTrace();
+				} 	
+			}
+			
+			if(leer2!=null) {
+				try {
+					leer.close();
+				} catch (IOException e) {
+					//para depurar
+					e.printStackTrace();
+				} 	
+			}
+		}
 		
 	}
 
