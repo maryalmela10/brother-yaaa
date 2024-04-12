0b4e7290baddd7201739b3ad75b0133754fadbdd
Mary Almela <maryalmela10@gmail.com>
2024-04-12 19:26:44 +0200

    j

---
+++ grafica/src/calculadora/Calculadora.java
@@ -0,0 +1,60 @@
+package calculadora;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+
+public class Calculadora extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Calculadora frame = new Calculadora();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Calculadora() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		contentPane.add(btnNewButton_3);
+		
+		JButton button = new JButton("New button");
+		contentPane.add(button);
+	}
+
+}
